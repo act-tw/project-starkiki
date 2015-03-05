393f165bd5f58b3cb7c73eaf9cd7da0af10dae09
tooto1985 <tooto1985@gmail.com>
2015-03-05 16:17:18 +0800

    modify

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 $(function() {
 	(function() {
+		function setZIndex() {
+			for (var i = 0, max = $(".start>img").length; i < max; i++) {
+				$(".start>img").eq(i).css("z-index", -(i + 1));
+			}
+		}
+		function run() {
+			if ($(".start>img:visible").length<=1) {
+				$(".start>img").eq(0).fadeIn(function() {
+					$(".start>img:hidden").show();
+				});
+			} else {
+				$(".start>img:visible").eq(0).fadeOut();	
+			}
+		}
+		setInterval(run, 3000);
 		$("body").css("overflow", "hidden").attr("onmousewheel", "return false;");
 		$(".start>.btn").click(function() {
 			$(".start").hide();
@@ -13,6 +28,7 @@ $(function() {
 				} else {
 					$(".start>img").removeAttr("width style").attr("height", "100%").css("margin-left", ($(window).width() - $(".start>img").width()) / 2);
 				}
+				setZIndex();
 			};
 			img.src = $(".start>img").attr("src");
 		}).resize();
