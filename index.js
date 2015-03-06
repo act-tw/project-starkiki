8198cc03b729793477fe3be5741e2e2e1dea5cc7
tooto1985 <tooto1985@gmail.com>
2015-03-06 10:33:26 +0800

    modify

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(function() {
 	(function() {
+		var timer = 3000;
+
 		function setZIndex() {
 			for (var i = 0, max = $(".start>img").length; i < max; i++) {
 				$(".start>img").eq(i).css("z-index", -(i + 1));
@@ -15,8 +17,6 @@ $(function() {
 				$(".start>img:visible").eq(0).fadeOut();
 			}
 		}
-		setInterval(run, 3000);
-		$("body").css("overflow", "hidden").attr("onmousewheel", "return false;");
 		$(".start>.btn").click(function() {
 			$(".start").hide();
 			$("body").css("overflow", "auto").removeAttr("onmousewheel");
@@ -33,24 +33,76 @@ $(function() {
 			};
 			img.src = $(".start>img").attr("src");
 		}).resize();
+		(function() {
+			$("body").css("overflow", "hidden").attr("onmousewheel", "return false;");
+			setInterval(run, timer);
+		})(); //init
 	})(); //start
 
 	(function() {
-		var $inbox = $(".index>.outbox>.inbox"),
+		var timer = 3000,
+			$outbox = $(".index>.outbox"),
+			$inbox = $outbox.find(">.inbox"),
 			$a = $inbox.find(">a"),
 			$img = $a.find(">img"),
 			img = new Image(),
-			html = "";
-
-		html = "<a href=\"" + $a.first().attr("href") + "\"><img src=\"" + $a.first().find(">img").attr("src") + "\"></a>";
+			html = "<a href=\"" + $a.first().attr("href") + "\"><img src=\"" + $a.first().find(">img").attr("src") + "\"></a>",
+			sid = null;
 		$inbox.append(html);
-		$inbox = $(".index>.outbox>.inbox");
+		$inbox = $outbox.find(">.inbox");
 		$a = $inbox.find(">a");
 		$img = $a.find(">img");
-
 		img.onload = function() {
-			var $outbox = $(".index>.outbox"),
-				$inbox = $outbox.find(">.inbox");
+			function run() {
+				var index;
+
+				if (parseInt($inbox.css("left")) <= -(img.width * ($img.length - 1))) {
+					$inbox.css("left", 0);
+				}
+				$inbox.animate({
+					"left": "-=" + img.width
+				});
+
+				index = Math.abs(parseInt($inbox.css("left")) / (img.width)) + 1;
+				if (index >= $img.length - 1) {
+					index = 0;
+				}
+				setNaviActive(index);
+			}
+
+			function navi() {
+				html = "";
+				for (var i = 0, max = $img.length - 1; i < max; i++) {
+					html += "<span";
+					if (i === 0) {
+						html += " class=\"active\"";
+					}
+					html += "></span>";
+				}
+				$outbox.prepend("<div class=\"navi\">" + html + "</div>");
+				$outbox.find(">.navi").css("margin-left", -($outbox.find(">.navi").width() / 2));
+			}
+
+			function setNaviActive(index) {
+				var $span = $outbox.find(">.navi>span");
+				$span.filter(".active").removeClass();
+				$span.eq(index).addClass("active");
+				console.log(index);
+			}
+			$outbox.mouseenter(function() {
+				if (sid !== null) {
+					clearInterval(sid);
+				}
+			}).mouseleave(function() {
+				sid = setInterval(run, timer);
+			});
+			$outbox.on("click", ">.navi>span", function() {
+				var index = $(this).index();
+				$inbox.animate({
+					"left": index * -(img.width)
+				});
+				setNaviActive(index);
+			});
 			$outbox.css({
 				"width": img.width,
 				"height": img.height
@@ -59,16 +111,8 @@ $(function() {
 				"width": img.width * $img.length,
 				"height": img.height
 			});
-
-			function run() {
-				if (parseInt($inbox.css("left")) <= -(img.width * ($img.length - 1))) {
-					$inbox.css("left", 0);
-				}
-				$inbox.animate({
-					"left": "-=" + img.width
-				});
-			}
-			setInterval(run, 3000);
+			navi();
+			sid = setInterval(run, timer);
 		}
 		img.src = $img.eq(0).attr("src");
 	})(); //index
